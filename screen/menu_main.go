bf50b3ce42999e1acbdb15a12225bc737fc7241a
Vladislavs Zamišļajevs <vzamislajevs@edu.riga.lv>
2022-05-12 10:06:04 +0300

    added fonts for classes

--- screen/menu_main.go
+++ screen/menu_main.go
@@ -21,18 +21,22 @@ func MainMenu(th *material.Theme, state *state.State) Screen {
 	return func(gtx layout.Context) (Screen, layout.Dimensions) {
 		matAddBut := material.Button(th, &add, "Add student")
 		matAddBut.Font = text.Font{Variant: "Mono", Style: text.Regular}
+		matAdd2But := material.Button(th, &add2, "Add classes")
+		matAdd2But.Font = text.Font{Variant: "Mono", Style: text.Regular}
 		matListBut := material.Button(th, &list, "List students")
 		matListBut.Font = text.Font{Variant: "Mono", Style: text.Regular}
+		matList2But := material.Button(th, &list2, "List classes")
+		matList2But.Font = text.Font{Variant: "Mono", Style: text.Regular}
 		matQuitBut := material.Button(th, &quit, "Quit")
 		matQuitBut.Font = text.Font{Variant: "Mono", Style: text.Regular}
-
+		
 		d := layout.Flex{Axis: layout.Vertical}.Layout(gtx,
 			layout.Rigid(rowInset(material.Button(th, &add, "Add student").Layout)),
 			layout.Rigid(rowInset(material.Button(th, &add2, "Add class").Layout)),
 			layout.Rigid(rowInset(material.Button(th, &list, "List students").Layout)),
 			layout.Rigid(rowInset(material.Button(th, &list2, "List classes").Layout)),
 			layout.Rigid(rowInset(material.Button(th, &quit, "Quit").Layout)),
-		)
+
 		if add.Clicked() {
 			return AddStudent(th, state), d
 		}
